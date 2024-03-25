4508e38aed5312e182fee400fedfab2fb57bae52
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-03-25 12:07:11 +0700

    add Model DataItem

---
+++ Craw_Data_From_URL/Model/DataItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Craw_Data_From_URL.Model
+{
+    public class DataItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+    }
+}
