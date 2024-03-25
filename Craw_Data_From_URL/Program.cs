a333b38c30e3ad00e0f1df5ea0072b061acb9632
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-03-25 12:06:54 +0700

    get value from html with class

--- Craw_Data_From_URL/Program.cs
+++ Craw_Data_From_URL/Program.cs
@@ -1,12 +1,17 @@
-﻿namespace Craw_Data_From_URL
+﻿using Craw_Data_From_URL.Services;
+using System.Text;
+
+namespace Craw_Data_From_URL
 {
     internal class Program
     {
-        static void Main(string[] args)
+        Crawl_Data_Service crawl = new Crawl_Data_Service();
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            string url = "https://antt.nguoiduatin.vn/co-phieu-hpx-chinh-thuc-duoc-giao-dich-tro-lai-tren-san-chung-khoan-tu-ngay-203-bien-do-dao-dong-len-toi-20-11255.html";
+            Crawl_Data_Service.GetDataAsync(url);
+            Console.ReadLine();
         }
-
-
     }
 }
---
+++ Craw_Data_From_URL/Services/Crawl_Data_Service.cs
@@ -0,0 +1,58 @@
+﻿using HtmlAgilityPack;
+
+namespace Craw_Data_From_URL.Services
+{
+    public class Crawl_Data_Service
+    {
+        public static async void GetDataAsync(string url)
+        {
+            try
+            {
+                var handler = new HttpClientHandler();
+                using (var client = new HttpClient(handler))
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string html = await response.Content.ReadAsStringAsync();
+                        HtmlDocument doc = new HtmlDocument();
+                        doc.LoadHtml(html);
+
+                        HtmlNodeCollection divNodes = doc.DocumentNode.SelectNodes("div");
+                        string xpath = $"//*[contains(@class,'article-title')]";
+                        if (doc.DocumentNode != null)
+                        {
+                            HtmlNode titleNode = doc.DocumentNode.SelectSingleNode($"//*[contains(@class,'article-title')]");
+                            HtmlNode descriptionNode = doc.DocumentNode.SelectSingleNode($"//*[contains(@class,'txt-head')]");
+                            HtmlNode contentNode = doc.DocumentNode.SelectSingleNode($"//*[contains(@class,'article-content')]");
+
+                            if (titleNode != null && descriptionNode != null && contentNode != null)
+                            {
+                                string title = titleNode.InnerText.Trim();
+                                Console.WriteLine($"Title: {titleNode.InnerText.Trim()}, Description : {descriptionNode.InnerText.Trim()}, Content: {contentNode.InnerText.Trim()}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Data not found");
+                            }
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Document node not found");
+                        }
+                    }
+                    else
+                    {
+                        throw new HttpRequestException($"HTTP request failed with status code: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Lỗi: {ex.Message}");
+                //return string.Empty;
+            }
+        }
+    }
+}
