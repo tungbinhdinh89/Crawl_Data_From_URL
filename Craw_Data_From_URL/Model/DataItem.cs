07c640c208c3938ee4ea968b5c08ea4168fb61ba
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-03-25 13:17:43 +0700

    fix model

--- Craw_Data_From_URL/Model/DataItem.cs
+++ Craw_Data_From_URL/Model/DataItem.cs
@@ -2,8 +2,9 @@
 {
     public class DataItem
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Content { get; set; }
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Content { get; set; }
     }
 }
