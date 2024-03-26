852579c6af8ef323fba2ee2402bd3d0e948861be
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-03-26 07:59:06 +0700

    re config dbContext and setup  connection string

---
+++ CrawlData.Lib/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Craw_Data_From_URL.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CrawlData.Lib.Data.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public DbSet<DataItem> DataItems { get; set; }
+    }
+}
